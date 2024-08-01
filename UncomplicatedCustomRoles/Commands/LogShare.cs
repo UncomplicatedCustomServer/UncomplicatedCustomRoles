96e7e763276bb383d58d32eb48fd8a32faf81db2
FoxWorn3365 <fede.cosma.ge@gmail.com>
2024-08-01 13:25:47 +0200

    ucrlogs bug

--- UncomplicatedCustomRoles/Commands/LogShare.cs
+++ UncomplicatedCustomRoles/Commands/LogShare.cs
@@ -5,6 +5,8 @@ using UncomplicatedCustomRoles.Manager;
 using System.Net.Http;
 using System.Collections.Generic;
 using Newtonsoft.Json;
+using System.Threading.Tasks;
+using Exiled.API.Features;
 
 namespace UncomplicatedCustomRoles.Commands
 {
@@ -30,18 +32,18 @@ namespace UncomplicatedCustomRoles.Commands
             }
 
             long Start = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+            response = "Loading the JSON content to share with the developers...";
 
-            HttpStatusCode Response = LogManager.SendReport(out HttpContent Content);
-            Dictionary<string, string> Data = JsonConvert.DeserializeObject<Dictionary<string, string>>(Plugin.HttpManager.RetriveString(Content));
+            Task.Run(() =>
+            {
+                HttpStatusCode Response = LogManager.SendReport(out HttpContent Content);
+                Dictionary<string, string> Data = JsonConvert.DeserializeObject<Dictionary<string, string>>(Plugin.HttpManager.RetriveString(Content));
 
-            if (Response is HttpStatusCode.OK && Data.ContainsKey("id"))
-            {
-                response = $"Successfully shared the UCR logs with the developers!\nSend this Id to the developers: {Data["id"]}\n\nTook {DateTimeOffset.Now.ToUnixTimeMilliseconds() - Start}ms";
-            }
-            else
-            {
-                response = $"Failed to share the UCR logs with the developers: Server says: {Response}";
-            }
+                if (Response is HttpStatusCode.OK && Data.ContainsKey("id"))
+                    Log.Info($"[ShareTheLog] Successfully shared the UCR logs with the developers!\nSend this Id to the developers: {Data["id"]}\n\nTook {DateTimeOffset.Now.ToUnixTimeMilliseconds() - Start}ms");
+                else
+                    Log.Info($"Failed to share the UCR logs with the developers: Server says: {Response}");
+            });
             
 
             return true;
