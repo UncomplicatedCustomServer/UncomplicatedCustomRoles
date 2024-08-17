715dafb59b1b0e1f5590344230375a74d7d92ce6
FoxWorn3365 <fede.cosma.ge@gmail.com>
2024-08-17 14:18:39 +0200

    version updoot

--- UncomplicatedCustomRoles/Plugin.cs
+++ UncomplicatedCustomRoles/Plugin.cs
@@ -23,7 +23,7 @@ namespace UncomplicatedCustomRoles
 
         public override string Author => "FoxWorn3365, Dr.Agenda";
 
-        public override Version Version { get; } = new(3, 6, 3);
+        public override Version Version { get; } = new(3, 6, 4);
 
         public override Version RequiredExiledVersion { get; } = new(8, 11, 0);
 
