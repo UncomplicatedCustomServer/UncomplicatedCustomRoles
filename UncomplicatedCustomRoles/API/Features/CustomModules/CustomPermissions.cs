6de7dba14e516041d970379090f59273bd5574a8
Federico Cosma <61429263+FoxWorn3365@users.noreply.github.com>
2025-04-15 00:04:35 +0200

    added new permission custom flag

---
+++ UncomplicatedCustomRoles/API/Features/CustomModules/CustomPermissions.cs
@@ -0,0 +1,15 @@
+﻿using Exiled.Permissions.Extensions;
+using System.Collections.Generic;
+
+namespace UncomplicatedCustomRoles.API.Features.CustomModules
+{
+    public class CustomPermissions : CustomModule
+    {
+        public override List<string> RequiredArgs => new()
+        {
+            "permissions"
+        };
+
+        internal string[] Permissions => Args.TryGetValue("permissions", out string permissions) ? permissions.Replace(" ", string.Empty).Split(',') : new string[] { };
+    }
+}
---
+++ UncomplicatedCustomRoles/Patches/PermissionPrefix.cs
@@ -0,0 +1,25 @@
+﻿using Exiled.API.Features;
+using Exiled.Permissions.Extensions;
+using HarmonyLib;
+using System;
+using UncomplicatedCustomRoles.API.Features;
+using UncomplicatedCustomRoles.API.Features.CustomModules;
+using UncomplicatedCustomRoles.Extensions;
+
+namespace UncomplicatedCustomRoles.Patches
+{
+    [HarmonyPatch(typeof(Permissions), nameof(Permissions.CheckPermission), new Type[] { typeof(Player), typeof(string) })]
+    public class PermissionPrefix
+    {
+        static bool Prefix(Player player, string permission, ref bool __result)
+        {
+            if (player.TryGetSummonedInstance(out SummonedCustomRole role) && role.GetModule(out CustomPermissions module) && module.Permissions.Contains(permission))
+            {
+                __result = true;
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
