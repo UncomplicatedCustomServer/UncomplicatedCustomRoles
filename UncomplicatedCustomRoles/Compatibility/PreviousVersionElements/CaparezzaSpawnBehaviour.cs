9f514a664c89f2de5a8942794797538b97b4da16
FoxWorn3365 <fede.cosma.ge@gmail.com>
2025-05-19 20:19:00 +0200

    updated compatibilitymanager for UCR v8.0.0 (LabAPI)

---
+++ UncomplicatedCustomRoles/Compatibility/PreviousVersionElements/CaparezzaSpawnBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using MapGeneration;
+using PlayerRoles;
+using System.Collections.Generic;
+using UncomplicatedCustomRoles.API.Enums;
+
+namespace UncomplicatedCustomRoles.Compatibility.PreviousVersionElements
+{
+#nullable enable
+
+    internal class CaparezzaSpawnBehaviour
+    {
+        public List<RoleTypeId> CanReplaceRoles { get; set; } = new()
+        {
+            RoleTypeId.ClassD
+        };
+
+        public int MaxPlayers { get; set; } = 10;
+
+        public int MinPlayers { get; set; } = 1;
+
+        public float SpawnChance { get; set; } = 60;
+
+        public SpawnType Spawn { get; set; } = SpawnType.RoomsSpawn;
+
+        public List<FacilityZone> SpawnZones { get; set; } = new();
+
+        public List<string> SpawnRooms { get; set; } = new()
+        {
+            "LCZ_ClassDSpawn"
+        };
+
+        public List<RoleTypeId> SpawnRoles { get; set; } = new()
+        {
+            RoleTypeId.ClassD
+        };
+
+        public List<string> SpawnPoints { get; set; } = new();
+
+        public PlayerPermissions[]? RequiredPermission { get; set; } = new PlayerPermissions[] { };
+    }
+}
