fc0a56e587f60ac6c5475956f3502f7236c0047a
MedveMarci <martonmende@gmail.com>
2026-01-27 10:11:14 +0100

    ReImplemented CustomPermissions module

--- UncomplicatedCustomRoles/API/Features/CustomModules/CustomPermissions.cs
+++ UncomplicatedCustomRoles/API/Features/CustomModules/CustomPermissions.cs
@@ -1,18 +1,46 @@
 ﻿/*
  * This file is a part of the UncomplicatedCustomRoles project.
- * 
+ *
  * Copyright (c) 2023-present FoxWorn3365 (Federico Cosma) <me@fcosma.it>
- * 
+ *
  * This file is licensed under the GNU Affero General Public License v3.0.
  * You should have received a copy of the AGPL license along with this file.
  * If not, see <https://www.gnu.org/licenses/>.
  */
 
-using System;
+using System.Collections.Generic;
+using LabApi.Features.Permissions;
+using UncomplicatedCustomRoles.Manager;
 
 namespace UncomplicatedCustomRoles.API.Features.CustomModules
 {
-    [Obsolete("Not available on LabAPI")]
     public class CustomPermissions : CustomModule
-    { }
+    {
+        public override List<string> RequiredArgs => new()
+        {
+            "permissions"
+        };
+
+        private string[] Permissions => StringArgs.TryGetValue("permissions", out string permissions) ? permissions.Replace(" ", string.Empty).Split(',') : new string[] { };
+        
+        public override void OnAdded()
+        {
+            var player = CustomRole.Player;
+            foreach (var permission in Permissions)
+            {
+                player?.AddPermissions(permission);
+            }
+            base.OnAdded();
+        }
+
+        public override void OnRemoved()
+        {
+            var player = CustomRole.Player;
+            foreach (var permission in Permissions)
+            {
+                player?.RemovePermissions(permission);
+            }
+            base.OnRemoved();
+        }
+    }
 }
