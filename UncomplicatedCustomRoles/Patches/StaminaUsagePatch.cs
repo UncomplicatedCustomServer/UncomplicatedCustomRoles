df8cb1dc929bf8d966012b4dd7d49759b7d44241
MedveMarci <martonmende@gmail.com>
2026-02-10 21:08:47 +0100

    Fixed Stamina Usage Patches

--- UncomplicatedCustomRoles/Patches/StaminaUsagePatch.cs
+++ UncomplicatedCustomRoles/Patches/StaminaUsagePatch.cs
@@ -1,8 +1,8 @@
 ﻿/*
  * This file is a part of the UncomplicatedCustomRoles project.
- * 
+ *
  * Copyright (c) 2023-present FoxWorn3365 (Federico Cosma) <me@fcosma.it>
- * 
+ *
  * This file is licensed under the GNU Affero General Public License v3.0.
  * You should have received a copy of the AGPL license along with this file.
  * If not, see <https://www.gnu.org/licenses/>.
@@ -10,40 +10,29 @@
 
 using HarmonyLib;
 using InventorySystem;
-using PlayerRoles.FirstPersonControl;
-using UncomplicatedCustomRoles.API.Features;
-using UncomplicatedCustomRoles.API.Features.Behaviour;
 using UncomplicatedCustomRoles.Extensions;
 
 namespace UncomplicatedCustomRoles.Patches
 {
-    [HarmonyPatch(typeof(Inventory), "get_StaminaUsageMultiplier")]
+    [HarmonyPatch(typeof(Inventory), nameof(Inventory.StaminaUsageMultiplier), MethodType.Getter)]
     public class StaminaUsagePatch
     {
-        public static bool Prefix(FpcStateProcessor __instance, ref float __result)
+        public static void Postfix(Inventory __instance, ref float __result)
         {
-            if (__instance.Hub.TryGetSummonedInstance(out SummonedCustomRole role) && role.Role.Stamina is StaminaBehaviour stamina)
-            {
-                __result *= stamina.Infinite ? 0 : stamina.UsageMultiplier;
-                return false;
-            }
-
-            return true;
+            if (!__instance._hub.TryGetSummonedInstance(out var role))
+                return;
+            __result *= role.Role.Stamina.Infinite ? 0 : role.Role.Stamina.UsageMultiplier;
         }
     }
 
-    [HarmonyPatch(typeof(Inventory), "get_StaminaRegenMultiplier")]
+    [HarmonyPatch(typeof(Inventory), nameof(Inventory.StaminaRegenMultiplier), MethodType.Getter)]
     public class StaminaRegenPatch
     {
-        public static bool Prefix(FpcStateProcessor __instance, ref float __result)
+        public static void Postfix(Inventory __instance, ref float __result)
         {
-            if (__instance.Hub.TryGetSummonedInstance(out SummonedCustomRole role) && role.Role.Stamina is StaminaBehaviour stamina)
-            {
-                __result *= stamina.RegenMultiplier;
-                return false;
-            }
-
-            return true;
+            if (!__instance._hub.TryGetSummonedInstance(out var role))
+                return;
+            __result *= role.Role.Stamina.RegenMultiplier;
         }
     }
 }
